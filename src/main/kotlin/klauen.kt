9d278b7494b10297baef3d590c40456f2e20b069
Jayson Reis <santosdosreis@gmail.com>
2017-10-10 22:17:13 +0200

    Add some docs

--- src/main/kotlin/klauen.kt
+++ src/main/kotlin/klauen.kt
@@ -1,11 +1,25 @@
 import java.util.concurrent.atomic.AtomicInteger
 
+/**
+ * Possible returns of Work.
+ */
 sealed class Work {
+    /**
+     * The queue is empty and there is no work.
+     */
     object Empty : Work()
+
+    /**
+     * Someone stole the job from the queue.
+     */
     object Abort : Work()
+
+    /**
+     * The actual data from the queue.
+     */
     class Data<out T>(val data: T) : Work() {
         override fun equals(other: Any?): Boolean {
-            if (other == null || other.javaClass != this.javaClass)
+            if (other == null || other::class != this::class)
                 return false
             other as Work.Data<*>
             return this.data == other.data
@@ -23,14 +37,23 @@ class Deque<T> {
     private var buffer: Buffer<T?> = Buffer()
 
     fun pushBottom(data: T) {
-        // val t = top.get()
+        val t = top.get()
         val b = bottom.get()
-        // val size = b - t
-        // Grow if size >= buffer.size - 1
-        buffer.put(b, data)
+        val size = b - t
+        // If size >= buffer.size - 1 it means that there is no space left to rotate so we have to "grow" the list
+        // and it is done by just by appending data
+        if (size >= buffer.size())
+            buffer.put(data)
+        else
+            buffer.put(b, data)
         bottom.incrementAndGet()
     }
 
+    /**
+     * Steal a work from buffer and it is thread safe.
+     * It can return 3 result Work.Empty when the queue is empty, Work.Abort when some other thread stole your data
+     * and Work.Data(T) with desired data.
+     */
     fun steal(): Work {
         val t = top.get()
         val b = bottom.get()
@@ -45,34 +68,53 @@ class Deque<T> {
     }
 }
 
+/**
+ * Buffer class which uses a list as circular array.
+ */
 class Buffer<T> {
     private var list: MutableList<T> = mutableListOf() // arrayList?
 
+    /**
+     * Actual size of the list.
+     */
+    fun size(): Int = list.size
+
+    /**
+     * Add a data to the circular list.
+     */
     fun put(index: Int, data: T) {
-        val calculatedIndex = if (index == 0) 0 else index % list.size
-        if (calculatedIndex >= list.size) {
-            list.add(calculatedIndex, data)
-        } else
-            list[calculatedIndex] = data
+        list[index % list.size] = data
     }
 
-    fun get(index: Int): T {
-        return list[index % list.size]
-    }
+    /**
+     * Append data to the list, used when there is not enough space to use it as circular.
+     */
+    fun put(data: T) = list.add(data)
+    fun get(index: Int): T = list[index % list.size]
 }
 
+/**
+ * Worker class which is not thread safe and should stay only in the task producer thread.
+ */
 class Worker<T>(private var deque: Deque<T>) {
     fun pushBottom(data: T) {
         deque.pushBottom(data)
     }
 }
 
+/**
+ * Stealer class which is safe to be shared between threads.
+ */
 class Stealer<T>(private val deque: Deque<T>) {
     fun steal(): Work {
         return this.deque.steal()
     }
 }
 
+/**
+ * Create a pair of worker and stealer where workers should not be shared between threads and stealers are free to be
+ * shared.
+ */
 fun <T> create(): Pair<Worker<T>, Stealer<T>> {
     val deque = Deque<T>()
     return Pair(Worker(deque), Stealer(deque))
--- src/test/kotlin/TestKlauen.kt
+++ src/test/kotlin/TestKlauen.kt
@@ -10,5 +10,13 @@ class TestDeque {
         assertEquals(Work.Data(1), stealer.steal())
         assertEquals(Work.Empty, stealer.steal())
         assertEquals(Work.Empty, stealer.steal())
+
+        for (i in 0..1000) {
+            worker.pushBottom(i)
+        }
+
+        for (i in 1000.downTo(0)) {
+            assertEquals(Work.Data(i), stealer.steal())
+        }
     }
 }
